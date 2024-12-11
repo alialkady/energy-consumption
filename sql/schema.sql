fba970b1b8f6ed7962c801b14ac7f6f536108ae3
alialkady <alialkady22@yahoo.com>
2024-12-11 16:48:17 +0200

    schema

---
+++ sql/schema.sql
@@ -0,0 +1,12 @@
+create DATABASE energy_consumption
+use energy_consumption
+
+    create table report (
+        name VARCHAR(20),
+        result DOUBLE
+    )
+
+
+select * from report
+
+drop Table report
